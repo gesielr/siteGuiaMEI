684851d1050ada83c209db0e5d6b5f4491ce8120
gesielr <carlosgesiell@gmail.com>
2025-05-21 11:15:46 -0300

    Adicionar base path para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/siteGuiaMEI/',
   server: {
     host: "::",
     port: 8080,
